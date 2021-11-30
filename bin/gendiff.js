85cab4be4faa223b4b6ecb6f165e246846643ca1
Shamil Khalikov <shamilist@mail.ru>
2021-11-30 22:20:08 +0300

    fix json file and gendiff name

---
+++ bin/gendiff.js
@@ -0,0 +1,9 @@
+#!/usr/bin/env node
+
+const { program } = require('commander');
+
+program
+  .description('Compares two configuration files and shows a difference.')
+  .version('0.0.1');
+
+  program.parse();
