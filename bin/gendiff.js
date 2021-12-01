b7d80210c33ec777ebf820bd5d967b52015496dd
Shamil Khalikov <shamilist@mail.ru>
2021-12-01 18:34:58 +0300

    fix json file step 1

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -5,5 +5,6 @@ const { program } = require('commander');
 program
   .description('Compares two configuration files and shows a difference.')
   .version('0.0.1');
+  .option();
 
   program.parse();
