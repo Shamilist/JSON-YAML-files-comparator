c32f0ee43cc8cd710a8e5bda909090db7268ae5f
Shamil Khalikov <shamilist@mail.ru>
2021-12-01 21:00:01 +0300

    done step 2

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -5,6 +5,8 @@ const { program } = require('commander');
 program
   .description('Compares two configuration files and shows a difference.')
   .version('0.0.1')
-  .option('-f, --format', 'output format');
+  .option('-f, --format <type>', 'output format')
+  .argument('<filepath1>')
+  .argument('<filepath2>');
 
   program.parse();
