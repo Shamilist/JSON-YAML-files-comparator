976d71819a79a511defdf6f2c1ea1d346729a1fa
Shamil Khalikov <shamilist@mail.ru>
2021-12-01 20:16:48 +0300

    step 2 add  -f -- format

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -4,7 +4,7 @@ const { program } = require('commander');
 
 program
   .description('Compares two configuration files and shows a difference.')
-  .version('0.0.1');
-  .option();
+  .version('0.0.1')
+  .option('-f, --format', 'output format');
 
   program.parse();
