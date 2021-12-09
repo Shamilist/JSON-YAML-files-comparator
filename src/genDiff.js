ea61e83b94033be96bd01b0197b55cea9a3921f1
Shamil Khalikov <shamilist@mail.ru>
2021-12-09 17:31:13 +0300

    step3 make jsonparse in genDigff.js

--- src/genDiff.js
+++ src/genDiff.js
@@ -2,25 +2,26 @@ import _ from 'lodash';
 
 const genDiff = (object1, object2) => {
     let resultObject = '';
-  
-    const commonKeys = _.union(_.keys(object1), _.keys(object2));
 
-    console.log("!!!", commonKeys);
-  
+    const jsonObj1 = JSON.parse(object1);
+    const jsonObj2 = JSON.parse(object2);
+
+    const commonKeys = _.union(_.keys(jsonObj1), _.keys(object2));
+
     for (const name of commonKeys) {
-        if (!_.has(object1, name)) { // если объект1 не содержит ключ - значит его добавили во второй
+        if (!_.has(jsonObj1, name)) { // если объект1 не содержит ключ - значит его добавили во второй
         resultObject = `${resultObject}
-+ ${name}: ${object2[name]}`;
-      } else if (!_.has(object2, name)) { // если объект2 не содержит ключ - значит его удалили во втором
++ ${name}: ${jsonObj2[name]}`;
+      } else if (!_.has(jsonObj2, name)) { // если объект2 не содержит ключ - значит его удалили во втором
         resultObject = `${resultObject}
-- ${name}: ${object1[name]}`;
-      } else if (object1[name] !== object2[name]) { // если свойства двух объектов с одинаковым ключом не равны - значит свойство поменяли
+- ${name}: ${jsonObj1[name]}`;
+      } else if (jsonObj1[name] !== jsonObj2[name]) { // если свойства двух объектов с одинаковым ключом не равны - значит свойство поменяли
         resultObject = `${resultObject}
-- ${name}: ${object1[name]}
-+ ${name}: ${object2[name]}`;
+- ${name}: ${jsonObj1[name]}
++ ${name}: ${jsonObj2[name]}`;
       } else {
         resultObject = `${resultObject}
-  ${name}: ${object1[name]}`; // объекты равны - значит ничего не меняли
+  ${name}: ${jsonObj1[name]}`; // объекты равны - значит ничего не меняли
       }
     }
     return  `{${resultObject}\n}`;
