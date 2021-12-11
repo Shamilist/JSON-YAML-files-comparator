1d8bd13cf7a221ec20288f15755e99019c0f0e7b
Shamil Khalikov <shamilist@mail.ru>
2021-12-11 15:24:21 +0300

    step 3 done

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -1,6 +1,9 @@
 #!/usr/bin/env node
 
 const genDiff = require('../src/genDiff.js');
+const fs = require('fs');
+const path = require('path');
+const process = require('process');
 const { program } = require('commander');
 
 program
@@ -10,6 +13,6 @@ program
   .argument('<filepath1>')
   .argument('<filepath2>')
   .action((filepath1, filepath2) => {
-    console.log(genDiff(filepath1, filepath2));
+    console.log(genDiff(fs.readFileSync(path.resolve( process.cwd(), filepath1)), fs.readFileSync(path.resolve( process.cwd(), filepath2))));
   })
   .parse();
--- src/genDiff.js
+++ src/genDiff.js
@@ -2,11 +2,11 @@ const _ = require('lodash');
 
 const genDiff = (object1 , object2) => {
     let resultObject = '';
-
+    
     const jsonObj1 = JSON.parse(object1);
     const jsonObj2 = JSON.parse(object2);
 
-    const commonKeys = (_.union(_.keys(object1), _.keys(object2))).sort();
+    const commonKeys = (_.union(_.keys(jsonObj1), _.keys(jsonObj2))).sort();
 
     for (const name of commonKeys) {
         if (!_.has(jsonObj1, name)) { // если объект1 не содержит ключ - значит его добавили во второй
