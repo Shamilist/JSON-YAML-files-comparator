d17bb8a4cddb5fd47be30dc30bc87e3f7a3551ed
Shamil Khalikov <shamilist@mail.ru>
2021-12-17 22:23:59 +0300

    fix tests

--- __tests__/genDiff.test.js
+++ __tests__/genDiff.test.js
@@ -13,16 +13,16 @@ const getFixturePath = (filename) => path.join(dirName, '..', '__fixtures__', fi
 const readFile = (filename) => fs.readFileSync((filename), 'utf-8'); // чтение файла
 
 test('genDiff json files', () => {
-  const file1 = readFile(getFixturePath('file1.json'));
-  const file2 = readFile(getFixturePath('file2.json'));
+  const file1 = getFixturePath('file1.json');
+  const file2 = getFixturePath('file2.json');
   const resultFile = readFile(getFixturePath('expected_test.txt'));
   const makeGendiff = genDiff(file1, file2);
   expect(makeGendiff).toEqual(resultFile);
 });
 
 test('genDiff yaml files', () => {
-  const file1 = readFile(getFixturePath('file1.yaml'));
-  const file2 = readFile(getFixturePath('file2.yaml'));
+  const file1 = getFixturePath('file1.yaml');
+  const file2 = getFixturePath('file2.yaml');
   const resultFile = readFile(getFixturePath('expected_test.txt'));
   const makeGendiff = genDiff(file1, file2);
   expect(makeGendiff).toEqual(resultFile);
