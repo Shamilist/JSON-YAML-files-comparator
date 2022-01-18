4aa971bfda5244393e6f11a5613e6e4476375006
Shamil Khalikov <shamilist@mail.ru>
2022-01-18 17:52:46 +0300

    add makeTree.js code

--- src/makeTree.js
+++ src/makeTree.js
@@ -1,1 +1,30 @@
-1
+import _ from 'lodash';
+
+const makeTree = (data1, data2) => {
+  const commonKeys = (_.union(_.keys(data1), _.keys(data2))).sort();
+
+  const result = commonKeys.map((key) => {
+    const value1 = data1[key];
+    const value2 = data2[key];
+
+    switch (true) {
+      case (!_.has(data1, key)):
+        return { key, type: 'added', value: value2 };
+      case (!_.has(data2, key)):
+        return { key, type: 'deleted', value: value1 };
+      case (_.isPlainObject(value1) && _.isPlainObject(value2)):
+        return { key, type: 'hasChildren', children: makeTree(value1, value2) };
+      case (!_.isEqual(value1, value2)):
+        return {
+          key, type: 'changed', beforeValue: value1, afterValue: value2,
+        };
+      case (_.isEqual(value1, value2)):
+        return { key, type: 'unchanged', value: value1 };
+      default:
+        throw new Error('Error of formatted');
+    }
+  });
+  return result;
+};
+
+export default makeTree;
